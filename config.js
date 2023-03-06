dc9dd3e46602c072c6ddc8179c98322d1065270b
BuiDatBk <118350748+BuiDatBk@users.noreply.github.com>
2023-03-06 11:04:20 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Chào em, bé yêu của anh!',
+    introDesc: `Anh vẫn còn nhớ như in ngày đó, cái ngày anh bị cướp đi một thứ rất quan trọng trong cuộc đời. Chính em đã lấy đi trái tim nguội lạnh của anh và cám ơn em đã sưởi ấm để nó lại tiếp tục loạn nhịp.`,
+    btnIntro: 'hihi',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích anh <33',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Anh dễ thương lại đẹp trai nữa <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Còn chờ gì nữa mà không inbox cho a đi nào kk.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/dat.rongvang.73' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
